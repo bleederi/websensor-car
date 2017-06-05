e8051397d9f9279200535f31836e1b6cf302f06c
Jesse Nieminen <jesse.nieminen@intel.com>
2017-06-05 09:30:18 +0300

    started sensor implementation

--- script.js
+++ script.js
@@ -26,6 +26,11 @@ speed = 5;
 angle = 0;
 mod = 0;
 
+var orientationMat = new Float64Array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);     //orientation
+var angles = {alpha:null, beta:null, gamma:null};
+var sensors = {};
+var sensorfreq = 60;    //for setting desired sensor frequency
+
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 car = new Image();
@@ -76,3 +81,44 @@ function keypress_handler(event) {
         angle += 5;
     }
 }
+
+function convert_orientation(orimatrix) {        //Convert orientation matrix to Euler angles
+        let alpha = 0;
+        let beta = 0;
+        let gamma = 0;
+        let r11 = orimatrix[0]
+        let r21 = orimatrix[4]
+        let r31 = orimatrix[8]
+        let r32 = orimatrix[9]
+        let r33 = orimatrix[10]
+        let betadivisor = Math.sqrt(Math.pow(r32,2) + Math.pow(r33,2));
+        if(r11 != 0 && r33 != 0 && betadivisor != 0) { //Can't divide by zero
+                alpha = Math.atan2(r21, r11);
+                beta = Math.atan2(-r31, (Math.sqrt(Math.pow(r32,2) + Math.pow(r33,2))));
+                gamma = Math.atan2(r32, r33);
+        }        
+        angles.alpha = alpha;
+        angles.beta = beta;
+        angles.gamma = gamma;
+        return angles;  //from -pi to pi
+}
+
+function startSensors() {
+        try {
+        //AbsoluteOrientationSensor
+        absoluteorientationsensor = new AbsoluteOrientationSensor({ frequency: sensorfreq});
+        sensors.AbsoluteOrientationSensor = absoluteorientationsensor;
+        absoluteorientationsensor.onchange = event => {
+                absoluteorientationsensor.populateMatrix(orientationMat);
+                angles = convert_orientation(orientationMat);
+                //console.log(angles);
+        }
+        absoluteorientationsensor.onerror = err => {
+          absoluteorientationsensor = null;
+          console.log(`Absolute orientation sensor ${err.error}`)
+        };
+        absoluteorientationsensor.start();
+        } catch(err) { console.log(err); }
+        sensors_started = true;
+        return sensors;
+        }
