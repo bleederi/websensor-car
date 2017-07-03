4a75956de1cf895b5abb21a2a20e92ad7ead7884
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-03 14:45:12 +0300

    Physijs test

--- script.js
+++ script.js
@@ -259,7 +259,7 @@ function move(camera, car) //Moves the car(camera)
                 }
                 //camera.position.z = camera.position.z - speed;
                 //car.position.z = car.position.z - speed;
-                car.rotation = (0, 0, 0);
+                car.rotation.set(0, 0, 0);
                 velocity = (0, 0, speed);
                 //car.velocity.z = speed;
                 car.setLinearVelocity(velocity);
