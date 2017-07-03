e42a516241923c40cc4c7a33b0d2423af8fbabd2
Jesse Nieminen <jesse.nieminen@intel.com>
2017-07-03 15:04:32 +0300

    Physijs works now

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ var sphereMesh = null;
 
 var x = 0;      //car x coordinate
 var y = 0;      //car y coordinate
-var speed = 10;        //0.1 for threeJS, 10 for Physijs
+var speed = 0.1;        //0.1 for threeJS, 10 for Physijs
 var angle = 0;
 var mod = 0;
 
@@ -260,7 +260,7 @@ function move(camera, car) //Moves the car(camera)
                 camera.position.z = camera.position.z - speed;
                 //car.position.z = car.position.z - speed;
                 //car.rotation.set(0, 0, 0);
-                velocity = ({x: car.getLinearVelocity().x, y: car.getLinearVelocity().y, z: -speed});
+                velocity = ({x: car.getLinearVelocity().x, y: car.getLinearVelocity().y, z: -speed*100});
                 //console.log(velocity);
                 //car.velocity.z = speed;
                 car.setLinearVelocity(velocity);
